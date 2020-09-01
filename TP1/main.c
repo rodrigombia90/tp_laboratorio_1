f08a1d83edc32ac1d05845b1bd01c906d977e508
Alumno_Invitado <rodrigombia90>
2020-09-01 11:00:31 -0300

    inicio tp

---
+++ TP1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("***Bienvenido a la calculadora***\n");
+    printf("A continuacion ingrese los dos operandos\n");
+
+    return 0;
+}
